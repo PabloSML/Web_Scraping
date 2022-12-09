325a61fff7a8ca4c009289c1a45c9f6478f71be3
Pablo Smolkin <psmolkin@itba.edu.ar>
2022-12-09 13:55:26 -0300

    Cleaning up in functions. Added Email notification

--- notify_final_mark.py
+++ notify_final_mark.py
@@ -1,13 +1,21 @@
+# OS and Python Imports
 from time import sleep
 from typing import Optional
-import requests
 import warnings
+# Web Scraping Imports
+import requests
 from bs4 import BeautifulSoup as soup #parses/cuts  the html
 from urllib.parse import urljoin
+# Desktop Notification Imports
 from plyer import notification
-from creds import userName, password
+# Email Notification Imports
+from email.message import EmailMessage
+import ssl
+import smtplib
+# Credential Imports
+from creds import userName, password, email_sender, email_password, email_reciever
 
-def revisar_nota(userName: str, password: str, subject_code: str) -> Optional[str]:
+def check_mark(userName: str, password: str, subject_code: str) -> Optional[str]:
 
     with requests.Session() as s:
 
@@ -98,23 +106,45 @@ def revisar_nota(userName: str, password: str, subject_code: str) -> Optional[st
                     span_nota = td_nota.find('div').find('span').string
                     return span_nota
 
-codigo_materia = "22.21"
-give_me_a_heart_attack = False
+def send_email(sender: str, password: str, reciever: str, subject_code: str):
+    subject = f"Nota de final - Materia {subject_code}"
+    body = """
+    ¡Tu nota del final está disponible!
+    """
 
-while (nota := revisar_nota(userName=userName, password=password, subject_code=codigo_materia)) is None:
+    em = EmailMessage()
+    em['From'] = sender
+    em['To'] = reciever
+    em['Subject'] = subject
+    em.set_content(body)
+
+    context = ssl.create_default_context()
+    
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(user=sender, password=password)
+        smtp.sendmail(from_addr=sender, to_addrs=reciever, msg=em.as_string())
+
+def desktop_notify(mark: str, subject_code: str, give_me_a_heart_attack=False):
+    if give_me_a_heart_attack:
+        my_message = f"Tu nota de {subject_code} es {mark}!!"
+    else:
+        my_message = f"Tu nota de {subject_code} es..."
+        print("Felicitaciones/Lo siento! Tu nota es un", mark)
+
+    notification.notify(
+        title = 'Está la nota!',
+        message = my_message,
+        app_name= 'SGA Notafire',
+        app_icon = "D:\VSCodeRepos\Web_Scraping\icon.ico",
+        timeout = 90000,
+        toast=False
+    )
+
+
+codigo_materia = "22.21"
+
+while (nota := check_mark(userName=userName, password=password, subject_code=codigo_materia)) is None:
     sleep(900)
 
-if give_me_a_heart_attack:
-    my_message = f"Tu nota de {codigo_materia} es {nota}!!"
-else:
-    my_message = f"Tu nota de {codigo_materia} es..."
-    print("Felicitaciones/Lo siento! Tu nota es un", nota)
-
-notification.notify(
-    title = 'Está la nota!',
-    message = my_message,
-    app_name= 'SGA Notafire',
-    app_icon = "D:\VSCodeRepos\Web_Scraping\icon.ico",
-    timeout = 90000,
-    toast=False
-)
+send_email(sender=email_sender, password=email_password, reciever=email_reciever, subject_code=codigo_materia)
+desktop_notify(mark=nota, subject_code=codigo_materia, give_me_a_heart_attack=False)
